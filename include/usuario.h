c837e381d063de677942eb4e4ad92a6c83a68a59
FalconPeek <lucaslindstrom808@gmail.com>
2025-10-18 04:20:53 -0300

    asd

--- include/usuario.h
+++ include/usuario.h
@@ -4,15 +4,17 @@
 
 typedef struct 
 {
-    tString nombreUsuario; // nombre del usuario
-    long segundosEscuchados; // segundos totales escuchados
-    int reproduccionesTotales; // número total de reproducciones
+    tString username; // nombre del usuario
+    long segundos_escucha_total; // segundos totales escuchados
+    int reproducciones_totales; // número total de reproducciones
 } tUsuario;
 
-void usuarioPrint(const tUsuario* u);
+void usuario_print(const tUsuario* u);
 
 typedef struct {
     int songId; // identificador de la canción
     int plays;  // número de reproducciones de la canción por el usuario
     long segundosEscuchados; // segundos escuchados de la canción por el usuario
 } tUserSongStats;
+
+#endif /* USUARIO_H */
